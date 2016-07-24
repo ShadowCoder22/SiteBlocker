d05519eea49a8e5c8893d7614059a8d3e40eb463
ShadowCoder22 <levester.chaney@gmail.com>
2016-07-24 14:02:17 -0700

    adding functionality to use chrome local storage

--- SiteBlocker.js
+++ SiteBlocker.js
@@ -1,38 +1,27 @@
 function blockListedSites() {
-	var i, blockList = getCookie("blockList"),
-		blockArray = blockList.split(",");
-	console.log(blockList);
-	for(i=0; i<blockArray.length;i++) {
-		if(blockArray[i] !== "" && window.location.href.match(".*"+blockArray[i]+".*")) {
-			console.log("match found");
-			window.location = chrome.extension.getURL("BlockedSite.html");
-		}
-	}
-}
-
-function getCookie(cname) {
-    var i, c, name = cname + "=", clist = document.cookie.split(';');
-    for(i = 0; i<clist.length; i++) {
-        c = clist[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return "";
+	chrome.storage.local.get("blockList", function( results ) {
+    	var i, blockArray=results.blockList.split(";");
+    	console.log( results.blockList );
+    	console.log( blockArray );
+    	for(i=0; i<blockArray.length; i++) {
+    		if(blockArray[i] !== "" && window.location.href.match(".*"+blockArray[i]+".*")) {
+    			console.log("match found");
+    			window.location = chrome.extension.getURL("BlockedSite.html");
+    		}
+    	}
+    });
 }
 
 window.onload=function(){
-	document.getElementById("blockListField").value=getCookie("blockList");
+	chrome.storage.local.get("blockList", function( results) {
+		document.getElementById("blockListField").value=results.blockList;
+	});
 	document.getElementById("saveListButton").onclick=function() {
-		var expires,
-			d = new Date(),
-			value = document.getElementById("blockListField").value;
-		d.setTime(d.getTime()  + (10*365*24*60*60));
-		expires = "expires="+ d.toUTCString();
-		document.cookie = "blockList" + "=" + value + "; " + expires + "; path=/";
+		chrome.storage.local.set({"blockList": document.getElementById("blockListField").value}, function() {
+			console.log('Data has been saved.');
+		});
+		blockListedSites();
+		window.close();
 	};
 }
 blockListedSites();
